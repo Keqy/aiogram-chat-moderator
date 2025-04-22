aa1193c1225d507624d9162c48ef9145685463fa
Keqy <keqy.cpp@gmail.com>
2025-04-23 00:56:01 +0300

    docs: Добавь шапку лицензии в файлы исходного кода

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+'''
+This Source Code Form is subject to the terms of the Mozilla
+Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'''
+
 from build_bot import bot, dp, scheduler
 from routers import new_members_router, poll_router, activity_router
 from moderate import ban_inactive_members
