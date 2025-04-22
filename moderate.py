2d50d8edaa0ea9dc3206ef2537e7a7c00aba4a42
Keqy <keqy.cpp@gmail.com>
2025-04-23 00:55:43 +0300

    docs: Добавь шапку лицензии в файлы исходного кода

--- moderate.py
+++ moderate.py
@@ -1,3 +1,9 @@
+'''
+This Source Code Form is subject to the terms of the Mozilla
+Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+'''
+
 from build_bot import bot
 from aiogram.types import ChatPermissions
 from build_bot import GROUP_ID #, userbot
